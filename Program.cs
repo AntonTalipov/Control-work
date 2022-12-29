fb44ddc5005e05bfc24a93627a592c1071f1b11a
«Anton» <ant.talipov@gmail.com>
2022-12-29 10:29:04 +0300

    fill array

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("type a length of array"); 
+int arrayLength = int.Parse(Console.ReadLine()); // ввод размера массива
+string [] FillArray (int arrayLength) // заполнение массива с клавиатуры
+{
+  string [] array = new string[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+      Console.WriteLine("type a word");
+      array [i] = Console.ReadLine(); 
+    }
+    return array;
+}
